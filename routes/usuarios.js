547192b42dd172a6df62b17e3a4cbc35f50e093d
Jeral <jeral.cris@gmail.com>
2022-01-10 13:55:02 -0600

    validando los campos del endpoin registro

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,12 +1,25 @@
 // Rutas para crear usuarios
 
 const { Router } = require('express');
+const { check } = require('express-validator');
+
 const { crearUsuario } = require('../controllers/usuarios');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 // Crear un usuario
 // api/auth
-router.post('/register', crearUsuario);
+router.post(
+   '/register',
+   [
+      //Midlewares
+      check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+      check('email', 'El correo es obligatorio').isEmail(),
+      check('password', 'El password debe contener al menos 6 caracteres').isLength({ min: 6 }),
+      validarCampos,
+   ],
+   crearUsuario
+);
 
 module.exports = router;
