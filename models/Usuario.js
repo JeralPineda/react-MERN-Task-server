fdbed57f249a4c27222ffa281eb2a200c2849850
Jeral <jeral.cris@gmail.com>
2022-01-10 12:05:16 -0600

    Definiendo un modelo de Usuarios con mongoose

---
+++ models/Usuario.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const UsuarioSchema = Schema({
+   nombre: {
+      type: String,
+      required: [true, 'El nombre es obligatorio'],
+      trim: true,
+   },
+   email: {
+      type: String,
+      required: [true, 'El correo es obligatorio'],
+      trim: true,
+      unique: true,
+   },
+   password: {
+      type: String,
+      required: [this.google === false || this.github === false, 'La contraseña es requerida.'],
+      trim: true,
+   },
+   registro: {
+      type: Date,
+      default: Date.now(),
+   },
+   estado: {
+      type: Boolean,
+      default: true,
+   },
+   google: {
+      type: Boolean,
+      default: false,
+   },
+   github: {
+      type: Boolean,
+      default: false,
+   },
+});
+
+// Limitamos la información de la petición a mostrar
+UsuarioSchema.methods.toJSON = function () {
+   const { __v, password, _id, ...usuario } = this.toObject();
+
+   //    Remplazamos el nombre de _id por uid
+   usuario.uid = _id;
+
+   return usuario;
+};
+
+module.exports = model('Usuario', UsuarioSchema);
