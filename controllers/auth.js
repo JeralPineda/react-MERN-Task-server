0787471c53596d88f71e2d9c009f152d31d4ee71
Jeral <jeral.cris@gmail.com>
2022-01-12 00:49:17 -0600

    Creando endpoint para iniciar sesion con google

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,13 +3,47 @@ const bcrypt = require('bcryptjs');
 
 const Usuario = require('../models/Usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
-const IniciarSesion = async (req, res = response) => {
+const iniciarSesion = async (req, res = response) => {
    res.json({
       msg: 'Iniciar sesión...',
    });
 };
 
+const iniciarSesionGoogle = async (req, res = response) => {
+   const { id_token } = req.body;
+
+   try {
+      const { nombre, email } = await googleVerify(id_token);
+
+      let usuario = await Usuario.findOne({ email });
+
+      // Validando si el correo ya existe
+      if (usuario) {
+         return res.status(400).json({
+            ok: false,
+            msg: 'El usuario ya existe',
+         });
+      }
+
+      res.json({
+         ok: true,
+         msg: 'Usuario creado correctamente',
+         nombre,
+         email,
+      });
+   } catch (error) {
+      console.log(error);
+
+      res.status(500).json({
+         ok: false,
+         msg: 'Hable con el administrador',
+      });
+   }
+};
+
 module.exports = {
-   IniciarSesion,
+   iniciarSesion,
+   iniciarSesionGoogle,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,12 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { IniciarSesion } = require('../controllers/auth');
+const { iniciarSesion, iniciarSesionGoogle } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
-// Crear un usuario
+// iniciar sesión con correo y password
 // api/auth
 router.post(
    '/',
@@ -18,7 +18,11 @@ router.post(
       check('password', 'El password debe contener al menos 6 caracteres').isLength({ min: 6 }),
       validarCampos,
    ],
-   IniciarSesion
+   iniciarSesion
 );
+
+// Iniciar sesión con google
+// api/usuarios/google
+router.post('/google', [check('id_token', 'El id_token es necesario').not().isEmpty(), validarCampos], iniciarSesionGoogle);
 
 module.exports = router;
